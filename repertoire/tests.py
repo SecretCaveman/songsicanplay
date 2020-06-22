8b47ba5561191f8826ebcd689dcc25923aacf5cb
SecretCaveman <dominik-schneider@gmx.de>
2020-06-22 22:27:31 +0200

    Added stub home view (stolen from tutorial) and first unit tests

--- repertoire/tests.py
+++ repertoire/tests.py
@@ -1,5 +1,6 @@
 from django.test import TestCase
 from django.urls import resolve
+from django.http import HttpRequest
 from repertoire.views import home_page
 
 class HomePageTest(TestCase):
@@ -7,3 +8,7 @@ class HomePageTest(TestCase):
     def test_root_url_redirects_to_home(self):
         found = resolve('/')
         self.assertEqual(found.func,home_page)
+
+    def test_uses_home_template(self):
+        response = self.client.get('/')
+        self.assertTemplateUsed('home.html')
